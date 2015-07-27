1d3c9f7767927b19e2aba01a7aec7a38c5ddea2a
Blade Barringer <blade@crookedneighbor.com>
2015-07-26 20:00:23 -0500

    Correct routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,12 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.send('(ಠ_ಠ)');
 });
 
 router.post('/webhook', (req, res, next) => {
-  console.log("hit webhook endpoint");
   let body = req.body;
-  console.log(body);
   res.sendStatus(200);
 });
 
