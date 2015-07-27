2486a41dbb6dea6b15cad20f80fa78d1a7cf153a
Blade Barringer <blade@crookedneighbor.com>
2015-07-26 20:15:56 -0500

    Send a status of 200

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('(ಠ_ಠ)');
+  res.send('(ಠ_ಠ)').status(200);
 });
 
 router.post('/webhook', (req, res, next) => {
