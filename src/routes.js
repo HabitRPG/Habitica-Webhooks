c94e78a059177dedef98247849e3407eacb89630
Blade Barringer <blade@crookedneighbor.com>
2015-07-27 22:53:48 -0500

    Finish routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 let webhook = require('./webhook');
+let s3 = require('./s3');
 
 router.get('/', (req, res, next) => {
   res.send('(ಠ_ಠ)').status(200);
@@ -13,12 +14,46 @@ router.post('/webhook', (req, res, next) => {
 
   if (isXHub && isXHubValid) {
     let body = req.body;
-    console.log(body);
+
+    let branch = config.get('GITHUB_BRANCH_TO_WATCH');
+    let files = _getFilesToUpload(body);
+
+    let repoName = body.repository.full_name;
+    let baseUrl = `https://raw.githubusercontent.com/${repoName}/${branch}/`;
+
+    if (files) {
+      _uploadFiles(files, baseUrl);
+    }
 
     res.sendStatus(200);
   } else {
     res.sendStatus(403);
   }
 });
+
+var _getFilesToUpload = (body) => {
+  let isCorrectBranch = webhook.verifyBranch(body.ref);
+
+  if(isCorrectBranch) {
+    return webhook.getWatchedFiles(body);
+  }
+
+  return false;
+}
+
+var _uploadFiles = (files, baseUrl) => {
+  _(files).each((file) => {
+    let fullUrl = `${baseUrl}${file}`;
+    let fileName = _getFileName(file);
+    s3.getFileFromUrlAndUpload(fullUrl, fileName);
+  }).value();
+}
+
+var _getFileName(file) {
+  let piecesOfPath = file.split('/');
+  let name = _.last(piecesOfPath);
+
+  return name;
+}
 
 module.exports = router;
