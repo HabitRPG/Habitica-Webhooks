03d47ec2f295456a35a15134b4e092bb996fd6c3
Blade Barringer <blade@crookedneighbor.com>
2015-07-28 08:47:34 -0500

    Change error

--- src/app.js
+++ src/app.js
@@ -24,12 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.send('( 　ﾟ,_ゝﾟ)').status(err.status || 500);
 });
-
 
 module.exports = app;
