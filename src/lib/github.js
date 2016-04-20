743ee5d1b46446f5f6491df42496833af710476c
Blade Barringer <blade@crookedneighbor.com>
2016-04-20 07:35:56 -0500

    chore: upgrade lodash

--- src/lib/github.js
+++ src/lib/github.js
@@ -3,7 +3,7 @@
 let _ = require('lodash');
 
 function getFiles (commits, type) {
-  let files = _(commits).pluck(type).flattenDeep().uniq().filter().value();
+  let files = _(commits).map(type).flattenDeep().uniq().filter().value();
 
   return files;
 }
