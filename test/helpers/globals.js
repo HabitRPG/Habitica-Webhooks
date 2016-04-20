97aa291d4b043b69f4820ab126e51440454012bb
Blade Barringer <blade@crookedneighbor.com>
2016-04-19 20:44:37 -0500

    chore: Set up env setter in test config

--- test/helpers/globals.js
+++ test/helpers/globals.js
@@ -8,3 +8,8 @@ chai.use(sinonChai);
 global.sinon = require('sinon');
 global.expect = require('chai').expect;
 global.sandbox = global.sinon.sandbox.create();
+
+// set up required env variables
+/* eslint-disable no-process-env */
+process.env.S3_SPRITES_DIRECTORY = 'path/to/s3/';
+/* eslint-enable no-process-env */
